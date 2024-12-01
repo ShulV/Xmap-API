e13a2157148581d0701881f731a3c87c36f90fb2
Victor Shulpov <vshulpov@gmail.com>
2024-12-01 09:51:24 -0500

    xmap-24 - рабочий вариант метода добавления спота с картинками

--- src/main/java/com/xmap_api/controllers/SpotController.java
+++ src/main/java/com/xmap_api/controllers/SpotController.java
@@ -1,13 +1,15 @@
 package com.xmap_api.controllers;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.xmap_api.dto.request.NewSpotDTO;
 import com.xmap_api.dto.response.DefaultSpotDTO;
+import com.xmap_api.dto.response.SpotIdDTO;
 import com.xmap_api.dto.response.SpotWithImageLinksDTO;
 import com.xmap_api.services.SpotService;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
 import java.util.List;
 import java.util.UUID;
@@ -16,9 +18,11 @@ import java.util.UUID;
 @RequestMapping("/api/v1/spot")
 public class SpotController {
     private final SpotService spotService;
+    private final ObjectMapper objectMapper;
 
-    public SpotController(SpotService spotService) {
+    public SpotController(SpotService spotService, ObjectMapper objectMapper) {
         this.spotService = spotService;
+        this.objectMapper = objectMapper;
     }
 
     @GetMapping("/all")
@@ -29,5 +33,13 @@ public class SpotController {
     @GetMapping("/{spotId}")
     public ResponseEntity<SpotWithImageLinksDTO> getSpotInfo(@PathVariable("spotId") UUID spotId) {
         return ResponseEntity.ok(spotService.getSpotWithImageLinks(spotId));
+    }
+
+    // Создание спота с картинкам для дальнейшей модерации
+    @PostMapping
+    public ResponseEntity<?> createSpotForModeration(@RequestParam("files") MultipartFile[] files,
+                                                     @RequestParam("spot") String spotJson) throws JsonProcessingException {
+        NewSpotDTO dto = objectMapper.readValue(spotJson, NewSpotDTO.class);//TODO сразу нельзя?
+        return ResponseEntity.ok(new SpotIdDTO(spotService.createSpotForModeration(dto, files)));
     }
 }
--- src/main/java/com/xmap_api/services/S3FileService.java
+++ src/main/java/com/xmap_api/services/S3FileService.java
@@ -9,6 +9,7 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
+import java.util.Arrays;
 import java.util.List;
 import java.util.UUID;
 
@@ -36,6 +37,14 @@ public class S3FileService {
     public void createSpotImage(MultipartFile file, UUID spotId) {
         UUID s3FileId =  s3FileDAO.insert(new S3File(file, DBCode.S3File.FileType.SPOT_IMAGE));
         spotS3FileService.insert(spotId, s3FileId);
+    }
+
+    @Transactional
+    public void createSpotImages(MultipartFile[] files, UUID spotId) {
+        List<S3File> s3Files = Arrays.stream(files)
+                .map(file -> new S3File(file, DBCode.S3File.FileType.SPOT_IMAGE))
+                .toList();
+        s3FileDAO.batchInsertAndLink(s3Files, spotId);
     }
 
     public DownloadedFileDTO downloadFile(UUID s3FileId) {
