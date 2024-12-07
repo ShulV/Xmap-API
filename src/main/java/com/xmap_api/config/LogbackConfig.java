89005f890f907b0493c6b51c567a3b2039ef39c7
Victor Shulpov <vshulpov@gmail.com>
2024-12-07 00:21:31 -0500

    xmap-28 - Add logback (настроил логгер в проекте)

---
+++ src/main/java/com/xmap_api/config/LogbackConfig.java
@@ -0,0 +1,8 @@
+package com.xmap_api.config;
+
+public class LogbackConfig {
+    public static class Logger {
+        public static final String MAIN_LOG = "MAIN_LOG";
+        public static final String SPOT_LOG = "SPOT_LOG";
+    }
+}
