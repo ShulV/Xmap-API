131d054d05b2d8df4eff21848d76603abc8b9d46
victor <vshulpov@gmail.com>
2025-07-26 15:42:04 +0700

    Xmap 36 - base spot page

--- src/main/java/com/xmap_api/controllers/SpotController.java
+++ src/main/java/com/xmap_api/controllers/SpotController.java
@@ -1,7 +1,9 @@
 package com.xmap_api.controllers;
 
 import com.xmap_api.services.SpotService;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
@@ -12,7 +14,8 @@ public class SpotController {
         this.spotService = spotService;
     }
     @GetMapping("/spots")
-    public String getSpotListPage() {
+    public String getSpotListPage(Model model) {
+        model.addAttribute("spotList", spotService.getDefaultAll(Pageable.ofSize(100)));
         return "spots";
     }
 }
