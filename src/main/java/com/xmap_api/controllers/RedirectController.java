397e8ed8ff81e1cb9c81e0f5c7376b24e2c0d98b
victor <vshulpov@gmail.com>
2025-08-17 18:54:02 +0700

    Xmap 36 - скрыл лишнее

---
+++ src/main/java/com/xmap_api/controllers/RedirectController.java
@@ -0,0 +1,13 @@
+package com.xmap_api.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class RedirectController {
+
+    @GetMapping({"", "/"})
+    public String redirectToSpots() {
+        return "redirect:/spots?pageNumber=0&pageSize=3&viewMode=YMAP";
+    }
+}
