4306fb5a6217568f6f1a455125b0d7d4156e0161
Shulpov Victor <vshulpov@gmail.com>
2026-02-15 23:46:03 +0700

    40 - успешная регистрация

--- backend-api/src/main/java/ru/spotic_api/controllers/SignUpController.java
+++ backend-api/src/main/java/ru/spotic_api/controllers/SignUpController.java
@@ -1,5 +1,7 @@
 package ru.spotic_api.controllers;
 
+import org.springframework.context.MessageSource;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 import ru.spotic_api.dto.request.NewUserDTO;
 import ru.spotic_api.services.SignUpService;
 import jakarta.validation.Valid;
@@ -10,13 +12,17 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 
+import java.util.Locale;
+
 @Controller
 public class SignUpController {
 
     private final SignUpService signUpService;
+    private final MessageSource messageSource;
 
-    public SignUpController(SignUpService signUpService) {
+    public SignUpController(SignUpService signUpService, MessageSource messageSource) {
         this.signUpService = signUpService;
+        this.messageSource = messageSource;
     }
 
     @GetMapping("/sign-up")
@@ -26,7 +32,8 @@ public class SignUpController {
     }
 
     @PostMapping("/sign-up")
-    public String registerUser(@Valid @ModelAttribute("user") NewUserDTO user, BindingResult bindingResult) {
+    public String registerUser(RedirectAttributes redirectAttributes,
+                               @Valid @ModelAttribute("user") NewUserDTO user, BindingResult bindingResult) {
         if (bindingResult.hasErrors()) {
             return "sign-up";
         }
@@ -34,6 +41,9 @@ public class SignUpController {
         if (bindingResult.hasErrors()) {
             return "sign-up";
         }
-        return "redirect:/sign-in?registered=true";
+
+        redirectAttributes.addFlashAttribute("message",
+                messageSource.getMessage("info.successful-registration", null, Locale.getDefault()));
+        return "redirect:/sign-in";
     }
 }
