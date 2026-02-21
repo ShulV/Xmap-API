3b634e48794dc36f267d0b841191a77ad1287016
Shulpov Victor <vshulpov@gmail.com>
2026-02-21 13:13:54 +0700

    удалил /build из git

--- backend-api/build/resources/main/db/changelog/0.1/20250718160200-add-user-enabled-default.sql
+++
@@ -1,5 +0,0 @@
---liquibase formatted sql
---changeset Shulpov Victor:20250718160200-add-user-enabled-default splitStatements:true
-alter table "user"
-    alter column enabled set default true;
-
--- backend-api/build/resources/main/db/changelog/0.1/20250719235200-rename-table.sql
+++
@@ -1,5 +0,0 @@
---liquibase formatted sql
---changeset Shulpov Victor:20250719235200-rename-table splitStatements:true
-
-alter table authorities
-    rename to authority;
